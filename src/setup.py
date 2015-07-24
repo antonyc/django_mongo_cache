287f1344e5854cc93cbdca1f7397e4577beea1d0
Chaporgin Anton <chapson@yandex-team.ru>
2015-07-24 17:21:51 +0300

    fixes

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django_mongo_cache',
+    name='mongodb_cache',
     version='1.0',
     packages=find_packages(),
     include_package_data=True,
